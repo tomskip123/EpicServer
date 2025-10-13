57a1f7ed3ea6b0c58e2c70c331150a0817eba288
tom peacock <tom.peacocksunited@gmail.com>
2025-10-13 18:51:26 +0100

    added query params utils

--- utils.go
+++ utils.go
@@ -3,12 +3,31 @@ package epicserver
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"net/url"
 )
 
+// String utils
 func GenerateToken(n int) (string, error) {
 	bytes := make([]byte, n)
 	if _, err := rand.Read(bytes); err != nil {
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+type QueryParam map[string][]string
+
+// Url utils
+func BuildQueryParams(baseUrl string, queryParams QueryParam) string {
+	base, _ := url.Parse(baseUrl)
+
+	params := url.Values{}
+	for idx, strArray := range queryParams {
+		for _, str := range strArray {
+			params.Add(idx, str)
+		}
+	}
+
+	base.RawQuery = params.Encode()
+	return base.String()
 }
